2bdf0a5a223c996eccae1dc5d5f7e4e32652c8e1
Ujjawal Rathore <ujjawalrathore30@gmail.com>
2023-12-09 14:17:18 +0530

    First Commit

--- src/Assignment1/ass.js
+++ src/Assignment1/ass.js
@@ -7,3 +7,29 @@ const companies = [
       console.log(companies[i].name);
   for(let i=0;i<companies.length;i++)
       console.log("headquarter of "+companies[i].name+" is "+companies[i].hq);
+
+
+function showCompanyDetails(){
+    var table = document.getElementById("companyTable");
+    var tbody = document.getElementById("companyBody");
+
+    tbody.innerHTML = "";
+    companies.forEach(function(company){
+        var row = tbody.insertRow();
+        var cell1 = tbody.insertCell(0);
+        var cell2 = tbody.insertCell(1);
+        var cell3 = tbody.insertCell(2);
+        
+        cell1.textContent = company.name;
+        cell2.textContent = company.hq;
+        cell3.textContent = company.indiaLocations;
+    });
+
+    table.style.display = "table";
+}
+
+
+
+
+
+
