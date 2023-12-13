f24539df007584c81ba3cd6ec35de8d02afbee94
Ujjawal Rathore <ujjawalrathore30@gmail.com>
2023-12-13 23:33:18 +0530

    1st & 2nd project better solution

--- src/Assignment1/ass.js
+++ src/Assignment1/ass.js
@@ -2,34 +2,38 @@ const companies = [
     {name: 'Intelliswift', hq: 'Newark, CA', indiaLocations: ['Pune', 'Bengaluru']},
     {name: 'Salesforce', hq: 'San Francisco, CA', indiaLocations: ['Hyderabad', 'Bengaluru', 'Mumbai', 'Gurugram', 'Jaipur']}
   ];
+
+  
+  //Task 1 Solution
   console.log(companies.length);
-  for(let i=0;i<companies.length;i++)
-      console.log(companies[i].name);
-  for(let i=0;i<companies.length;i++)
-      console.log("headquarter of "+companies[i].name+" is "+companies[i].hq);
-
-
-function showCompanyDetails(){
-    var table = document.getElementById("companyTable");
-    var tbody = document.getElementById("companyBody");
-
-    tbody.innerHTML = "";
-    companies.forEach(function(company){
-        var row = tbody.insertRow();
-        var cell1 = tbody.insertCell(0);
-        var cell2 = tbody.insertCell(1);
-        var cell3 = tbody.insertCell(2);
-        
-        cell1.textContent = company.name;
-        cell2.textContent = company.hq;
-        cell3.textContent = company.indiaLocations;
+  companies.forEach(function(company) {
+    console.log(company.name + ': ' + company.hq);
+  });
+  
+  
+  //Task 2 Solution
+  function showInfo() {
+    //Step 1
+    const companyInfoDiv = document.getElementById('companyInformation');
+  
+    //Step 2 - Business Logic
+    let outputHtmlSnippet = '';
+    outputHtmlSnippet += '<h3>Number of companies: ' + companies.length + '</h3>';
+    outputHtmlSnippet += '<ol>'
+  
+    //One way to iterate
+    for (let i = 0; i < companies.length; i++) {
+      let company = companies[i];
+      outputHtmlSnippet += '<li>' + company.name + ' - ' + company.hq + ' - ' + company.indiaLocations.join(', ') + '</li>';
+    }
+  
+    //Better way to iterate
+    companies.forEach(function(company) {
+      outputHtmlSnippet += '<li>' + company.name + ' - ' + company.hq + ' - ' + company.indiaLocations.join(', ') + '</li>';
     });
-
-    table.style.display = "table";
-}
-
-
-
-
-
-
+  
+    outputHtmlSnippet += '</ol>'
+  
+    //Step 3
+    companyInfoDiv.innerHTML = outputHtmlSnippet;
+  }
